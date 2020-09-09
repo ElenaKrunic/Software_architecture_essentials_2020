012f965c093006bf21b7bcbc3a1ccdbe0215a4c6
ElenaKrunic <elenakrunic@gmail.com>
2020-09-09 19:25:22 +0200

    Inicijalan komit

---
+++ demo/src/main/java/com/projekat/demo/Aplikacija.java
@@ -0,0 +1,13 @@
+package com.projekat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplikacija {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aplikacija.class, args);
+	}
+
+}
