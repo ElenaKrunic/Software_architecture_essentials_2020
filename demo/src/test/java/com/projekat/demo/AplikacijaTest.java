8a7361171e491aea1bb16d2eab5f5ffaedca0f50
draganpetrovic <draganmop@gmail.com>
2020-09-22 14:02:01 +0200

    .

---
+++ demo/src/test/java/com/projekat/demo/AplikacijaTest.java
@@ -0,0 +1,14 @@
+package com.projekat.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AplikacijaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
